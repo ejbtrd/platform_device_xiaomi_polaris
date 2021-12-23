8c4608ba37c7f57b3f7d69f16ba259c173701e4b
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-12-23 09:07:32 +0100

    polaris: ProjectStreak bringup

--- streak_polaris.mk
+++ streak_polaris.mk
@@ -6,25 +6,19 @@
 
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
-# Inherit some common PixelExperience stuff.
-EVO_MAINTAINER := jjpprrrr
-EVO_SUPPORT_URL := https://t.me/EvolutionXPolaris
-EVO_DONATE_URL := https://paypal.me/jjpprrrr
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-DEVICE_REQUIRES_CARRIER_APPS := true
-
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common ProjectStreak stuff.
+$(call inherit-product, vendor/streak/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_polaris
+PRODUCT_NAME := streak_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
+
+# ProjectStreak properties
+STREAK_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
 
 BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
 
